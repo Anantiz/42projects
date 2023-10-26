fe1559d75bb26800404f12dda4f89468fceece8b
Antoine Urban <aurban@k0r4p9.42mulhouse.fr>
2023-10-26 18:15:21 +0200

    Make in doing

--- printf/libft/ft_calloc.c
+++ printf/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: aurban <aurban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 13:08:06 by aurban            #+#    #+#             */
-/*   Updated: 2023/10/26 15:55:14 by aurban           ###   ########.fr       */
+/*   Updated: 2023/10/26 16:56:01 by aurban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	max;
 
 	max = 0;
-	if (nmemb == 0 || size == 0 || ((max - 1) / size < nmemb))
+	if (((max - 1) / size < nmemb))
 		return (malloc(0));
 	ptr = malloc(nmemb * size);
 	if (ptr)
