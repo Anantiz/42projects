ba14c44872a7e672c593266f9597f77268082940
Antoine Urban <aurban@k2r2p2.42mulhouse.fr>
2023-10-31 09:55:21 +0100

    printf right

--- printf/libftprintf/ft_printf.c
+++ printf/libftprintf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aurban <aurban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:15:02 by aurban            #+#    #+#             */
-/*   Updated: 2023/10/29 06:34:54 by aurban           ###   ########.fr       */
+/*   Updated: 2023/10/31 09:52:58 by aurban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,31 @@ void	ft_flush(int fd, char *buffer, t_bd *bd)
 	bd->offset = 0;
 }
 
-static void	init_bd(t_bd *bd)
+static const char	*rf2(t_bd *bd, const char *s, char *buffer, va_list *args)
 {
-	bd->offset = 0;
-	bd->written = 0;
+	int	temp_read;
+
+	if (bd->offset == SBUFSIZ)
+		ft_flush(1, buffer, bd);
+	if (*s == '\n')
+	{
+		buffer[bd->offset++] = *(s++);
+		ft_flush(1, buffer, bd);
+	}
+	else if (*s == '%')
+	{
+		temp_read = 1;
+		while (s[temp_read] == ' ')
+			temp_read++;
+		if (ft_arg_to_buffer(buffer, bd, args, s[temp_read]) == -1)
+			ft_send_char(buffer, bd, *s);
+		else
+			s += temp_read;
+		s++;
+	}
+	else
+		buffer[bd->offset++] = *(s++);
+	return (s);
 }
 
 static int	read_format(const char *s, va_list *args)
@@ -33,24 +54,11 @@ static int	read_format(const char *s, va_list *args)
 	char	buffer[SBUFSIZ];
 	t_bd	bd;
 
-	init_bd(&bd);
+	bd.offset = 0;
+	bd.written = 0;
 	while (*s)
 	{
-		if (bd.offset == SBUFSIZ)
-			ft_flush(1, buffer, &bd);
-		if (*s == '\n')
-		{
-			buffer[bd.offset++] = *(s++);
-			ft_flush(1, buffer, &bd);
-		}
-		else if (*s == '%')
-		{
-			if (ft_arg_to_buffer(buffer, &bd, args, *(++s)))
-				ft_send_char(buffer, &bd, *s);
-			s++;
-		}
-		else
-			buffer[bd.offset++] = *(s++);
+		s = rf2(&bd, s, buffer, args);
 	}
 	ft_flush(1, buffer, &bd);
 	return (bd.written);
